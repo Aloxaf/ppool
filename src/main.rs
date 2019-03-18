cdd55b71c4043a9be0dbc3bde447662094b737d0
Aloxaf <aloxafx@gmail.com>
2019-03-18 23:41:19 +0800

    [Add] 使用 lazy_static 避免多次调用 app_dir

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use app_dirs::*;
 use clap::{load_yaml, App};
 use failure::{format_err, Error};
+use lazy_static::lazy_static;
 use log::{debug, info};
 use ppool::{proxy_pool::*, *};
 use std::fs::File;
@@ -16,23 +17,23 @@ const APP_INFO: AppInfo = AppInfo {
     author: "Aloxaf",
 };
 
-// TODO: lazy_static!
-fn get_data_path() -> PathBuf {
-    let mut data_path = app_dir(AppDataType::UserData, &APP_INFO, "proxy_list")
-        .expect("无法创建 UserData 目录");
-    data_path.push("proxies.json");
-    data_path
+// app_dir 会自动创建所需目录(如果不存在的话
+lazy_static! {
+    static ref DATA_PATH: PathBuf = {
+        let mut data_path = app_dir(AppDataType::UserData, &APP_INFO, "proxy_list")
+            .expect("无法创建 UserData 目录");
+        data_path.push("proxies.json");
+        debug!("data_path: {:?}", &data_path);
+        data_path
+    };
 }
 
 fn init_proxy_pool() -> AProxyPool {
     // 读取(可能的)上次的数据
     info!("正在读取缓存");
-    // app_dir 会自动创建所需目录(如果不存在的话
-    let data_path = get_data_path();
-    debug!("data_path: {:?}", &data_path);
 
     // 存在 proxies.json 的话, 读取 & 反序列化之
-    let proxy_pool = match File::open(&data_path) {
+    let proxy_pool = match File::open(DATA_PATH.clone()) {
         Ok(file) => serde_json::from_reader(file).unwrap(),
         Err(err) => {
             // 打开失败时, 可能是不存在, 也可能是其他问题, 此处输出错误信息便于调试
@@ -116,7 +117,6 @@ fn run() -> Result<(), Error> {
 
         // 代理验证线程
         let checker_thread = {
-            let data_path = get_data_path();
             let proxy_pool = proxy_pool.clone();
             let reload = reload.clone();
             thread::spawn(move || {
@@ -127,7 +127,7 @@ fn run() -> Result<(), Error> {
                     // TODO: 这个"备份"也单独开一个线程?
                     info!("写入到磁盘");
                     let data = serde_json::to_string_pretty(&proxy_pool).expect("无法序列化");
-                    let mut file = File::create(&data_path).expect("无法创建文件");
+                    let mut file = File::create(DATA_PATH.clone()).expect("无法创建文件");
                     file.write_all(data.as_bytes()).expect("无法写入");
 
                     info!("等待{}秒再次验证...", checker_config.interval);
